5865715eb51401021597fc53a2a1840f24e5538d
NguyenCuong <nguyencuong@Kakashis-MacBook-Pro-5.local>
2018-09-19 16:46:53 +0700

    no message

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ then
     sudo yum install monit -y;
     sudo cp ./monit/monitrc /etc/monitrc -f;
     sudo cp ./monit/monit.conf /etc/monit.conf -f;
-    sudo cp ./monit/monit.d/* /etc/monit.d/ -f;
     sudo chmod 0700 /etc/monitrc;
     sudo systemctl start monit.service;
 fi
+
+sudo cp ./monit/monit.d/* /etc/monit.d/ -f;
+sudo systemctl reload monit.service;
 
