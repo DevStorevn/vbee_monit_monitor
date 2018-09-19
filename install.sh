135137759d23490d381cdad7115ef2c3cb24dc60
NguyenCuong <nguyencuong@Kakashis-MacBook-Pro-5.local>
2018-09-19 16:20:59 +0700

    no message

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#rm -rf /etc/monit.conf;rm -rf /etc/monitrc;
 file="/etc/monit.conf"
 if [ ! -f "$file" ] 
 then
     echo monit not start, I will start it;
     sudo yum install monit -y;
+    sudo cp ./monit/monitrc /etc/monitrc -f;
     sudo cp ./monit/monit.conf /etc/monit.conf -f;
     sudo chmod 0700 /etc/monitrc;
     sudo systemctl start monit.service;
