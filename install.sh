851e3cb144f82f8bb9da896aa6609c6d9aaa1676
NguyenCuong <nguyencuong@Kakashis-MacBook-Pro-5.local>
2018-09-20 00:29:11 +0700

    no message

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
-#rm -rf /etc/monit.conf;rm -rf /etc/monitrc;
-file="/etc/monit.conf"
-if [ ! -f "$file" ] 
-then
-    echo monit not start, I will start it;
-    sudo yum install monit -y;
-    sudo cp ./monit/monitrc /etc/monitrc -f;
-    sudo cp ./monit/monit.conf /etc/monit.conf -f;
-    sudo chmod 0700 /etc/monitrc;
-    sudo systemctl start monit.service;
+sudo chmod +x /home/tts/monit/bin/monit;
+sudo chown root:root /home/tts/monit/conf/monitrc;
+sudo chmod 0700 /home/tts/monit/conf/monitrc;
+sudo chmod +x /home/tts/monit/monit2telegram.sh
+sudo chmod +x /home/tts/monit/sendtelegram.sh
+
+if ps aux | grep "monit -c /home/tts/monit/conf/monitrc" | grep -v grep >/dev/null ; then
+	echo "monit ok";
+else
+        sh /home/tts/monit/bin/monit -c /home/tts/monit/conf/monitrc
 fi
-sudo cp ./monit/monitrc /etc/monitrc -f;
-sudo cp ./monit/monit.conf /etc/monit.conf -f;
-sudo cp ./monit/monit.d/* /etc/monit.d/ -f;
-sudo systemctl reload monit.service;
+
+#./bin/monit -c /home/tts/monit/conf/monitrc  restart all
+#./bin/monit stop -c /home/tts/monit/conf/monitrc all
+
+#rm -rf /etc/monit.conf;rm -rf /etc/monitrc;
+# file="/etc/monit.conf"
+# if [ ! -f "$file" ] 
+# then
+#     echo monit not start, I will start it;
+#     sudo yum install monit -y;
+#     sudo cp ./monit/monitrc /etc/monitrc -f;
+#     sudo cp ./monit/monit.conf /etc/monit.conf -f;
+#     sudo chmod 0700 /etc/monitrc;
+#     sudo systemctl start monit.service;
+# fi
+# sudo cp ./monit/monitrc /etc/monitrc -f;
+# sudo cp ./monit/monit.conf /etc/monit.conf -f;
+# sudo cp ./monit/monit.d/* /etc/monit.d/ -f;
+# sudo systemctl reload monit.service;
 
