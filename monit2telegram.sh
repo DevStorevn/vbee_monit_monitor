0de5fc8e1b8a6c7f84f5db2f6e455615140c8948
NguyenCuong <nguyencuong@Kakashis-MacBook-Pro-5.local>
2020-10-22 00:48:24 +0700

    no message

--- monit2telegram.sh
+++ monit2telegram.sh
@@ -4,4 +4,4 @@
 #
 # Depends on having `/usr/local/bin/sendtelegram` installed and a config file in `/etc/telegramrc`
 #
-sh /etc/sendtelegram.sh -c /etc/telegramrc -m "Cảnh Báo $MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
+sh /etc/sendtelegram.sh -c /etc/telegramrc -m "$MONIT_SERVICE - $MONIT_EVENT at $MONIT_DATE on $MONIT_HOST: $MONIT_ACTION $MONIT_DESCRIPTION."
